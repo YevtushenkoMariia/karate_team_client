2d77a12daccb92208703bb30a326b0b8dd5de9dd
MariiaYevtushenko <yevtushenko.mariia2005@gmail.com>
2026-08-02 12:40:21 +0300

    Fix: axios env var for back connection

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -3,11 +3,11 @@ import { type LoginRequest } from "../types/auth.types";
 import { type RegisterRequest } from "../types/auth.types";
 
 export const login = async (credentials: LoginRequest) => {
-  const response = await api.post("/auth/login", credentials);
+  const response = await api.post("/api/auth/login", credentials);
   return response.data;
 };
 
 export const register = async (requestData: RegisterRequest) => {
-  const response = await api.post("/auth/register", requestData);
+  const response = await api.post("/api/auth/register", requestData);
   return response.data;
 };
