e856f4f22666add6afca7dc7e469deb084aaff13
MariiaYevtushenko <yevtushenko.mariia2005@gmail.com>
2026-08-02 13:13:00 +0300

    Add vercel json and logout btn

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,5 +1,27 @@
 import PageShell from "../components/PageShell";
+import { useNavigate } from "react-router-dom";
 
-export default function Statistics() {
-  return <PageShell title="Налаштування" />;
+export function SettingInside() {
+  const navigate = useNavigate();
+
+const handleLogout = () => {
+  localStorage.removeItem("token");
+  localStorage.removeItem("user");
+  navigate("/login");
+}
+
+  return <>
+
+  <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+  onClick={handleLogout}>
+    Log out 
+  </button>
+
+
+
+  </>
+}
+
+export default function Settings() {
+  return <PageShell title="Налаштування" children={<SettingInside />} />;
 }
