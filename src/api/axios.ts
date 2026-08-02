bc4251fe29c99a15af51d588e548336391a595d3
MariiaYevtushenko <yevtushenko.mariia2005@gmail.com>
2026-08-02 12:47:53 +0300

    Change env var name

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.KARATE_API_URL,
+    baseURL: import.meta.env.VITE_API_URL,
 });
 
 api.interceptors.request.use((config) => {
