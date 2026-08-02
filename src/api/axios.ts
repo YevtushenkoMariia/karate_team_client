3e6755b58fd7c652ae7047a5bd6cccf4d44558ca
MariiaYevtushenko <yevtushenko.mariia2005@gmail.com>
2026-08-02 12:51:16 +0300

    log url

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -6,6 +6,8 @@ const api = axios.create({
 
 api.interceptors.request.use((config) => {
 
+    console.log("env.VITE_API_URL = ", import.meta.env.VITE_API_URL);
+
     const token = localStorage.getItem("token");
 
     if (token) {
